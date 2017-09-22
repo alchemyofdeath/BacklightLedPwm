55ce7a4d738a89152f6cc4bab815e23a25a5a9ab
alchemyofdeath <doru_metalguitar@yahoo.com>
2017-09-22 22:11:43 +0300

    Update BacklightLedPwm.ino

--- BacklightLedPwm.ino
+++ BacklightLedPwm.ino
@@ -9,6 +9,6 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  adc_value = analogRead( adcPin );
-  analogWrite( ledPin ,( int )( ( ( long ) adc_value * 255L ) / 1024L ) );
+  adc_value = analogRead( adcPin ); // read 10 bit adc value from I/O pin 3
+  analogWrite( ledPin ,( int )( ( ( long ) adc_value * 255L ) / 1024L ) ); // convert 10 bit to 8 bit, write to PWM 
 }
