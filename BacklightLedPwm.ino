a1f25f0b5f0ebf51ab9b03bc1052d03a1697878e
alchemyofdeath <doru_metalguitar@yahoo.com>
2017-09-22 22:10:16 +0300

    Add files via upload
    First version

---
+++ BacklightLedPwm.ino
@@ -0,0 +1,14 @@
+int ledPin = 4;
+int adcPin = 3;
+int adc_value;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  adc_value = analogRead( adcPin );
+  analogWrite( ledPin ,( int )( ( ( long ) adc_value * 255L ) / 1024L ) );
+}
